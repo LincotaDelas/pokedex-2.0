c133400926ccc424733841bd6aba46285b91b036
LincotaDelas <155613769+LincotaDelas@users.noreply.github.com>
2024-01-05 03:04:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -77,21 +77,21 @@ const getPokemonData = async (id) => {
 
 // Função que cria uma carta (elemento HTML) para um Pokémon com base nos dados recebidos
 const createPokemonCard = (poke) => {
-    // Cria um elemento <div> para representar a carta do Pokémon
     const card = document.createElement('div');
     card.classList.add("pokemon");
 
-    // Extrai informações relevantes do objeto poke
-    const name = poke.name[0].toUpperCase() + poke.name.slice(1);
+    // Extrai o primeiro nome do objeto poke
+    const firstSpaceIndex = poke.name.indexOf(' '); // Encontra a posição do primeiro espaço
+    const truncatedName = poke.name.slice(0, firstSpaceIndex !== -1 ? firstSpaceIndex : undefined); // Pega o primeiro nome
+
+    const name = truncatedName[0].toUpperCase() + truncatedName.slice(1);
     const id = poke.id.toString().padStart(3, "0");
     const pokeTypes = poke.types.map(type => type.type.name);
     const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1);
     const color = colors[type];
 
-    // Define o fundo da carta com base no tipo do Pokémon
     card.style.backgroundColor = color;
 
-    // Cria a estrutura HTML interna da carta
     const pokemonInnerHtml = `
         <div class="imgContainer">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">
@@ -102,12 +102,10 @@ const createPokemonCard = (poke) => {
             <small class="type">Tipo:<span>${type}</span></small>
         </div>`;
 
-    // Define o conteúdo HTML da carta
     card.innerHTML = pokemonInnerHtml;
-
-    // Adiciona a carta ao contêiner principal no HTML
     pokeContainer.appendChild(card);
 }
+
 
 // Chama a função principal para buscar e criar cartas para todos os Pokémon
 fetchAllPokemons();
